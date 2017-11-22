8d193b2c5e37096988b48ee0512285bc511d0d09
Kgotso Koete <kgkoete@gmail.com>
2017-11-22 21:58:44 +0200

    Ch2 adding gear inches method

--- POODR.py
+++ POODR.py
@@ -1,11 +1,18 @@
 class Gear:
 	# initialize
-	def __init__(self, chainring, cog):
+	def __init__(self, chainring, cog, rim, tire):
 		self.chainring = chainring
 		self.cog = cog
+		self.rim = rim
+		self.tire = tire
 
 	# calculate the ratio
 	def ratio(self):
 		return self.chainring / float(self.cog)
 
-print(Gear(52, 11).ratio())
+	# calculate gear inches
+	def gear_inches(self):
+		return self.ratio() * (self.rim + (self.tire * 2))
+
+print(Gear( 52 , 11 , 26 , 1.5).gear_inches())
+print(Gear( 52 , 11 , 24 , 1.25).gear_inches())
