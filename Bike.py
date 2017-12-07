159f8b95c65fa77087e8438554a5d3a006c396f4
Kgotso Koete <kgkoete@gmail.com>
2017-12-07 20:59:07 +0200

    Ch 4 creating a class without Duck Types

--- Bike.py
+++ Bike.py
@@ -1,14 +1,57 @@
-#create a GearWrapper class to interface with Gear created in external module
-class GearWrapper(object):
-    @classmethod
-    def gear(self, **kwargs):
-        from POODR import Gear
-        # return an instance of Gear
-        return Gear(kwargs.get('chainring'), kwargs.get('cog'), kwargs.get('wheel'))
+'''
+Moving methods from Gear to Wheel
+'''
+
+class Gear:
+	# initialize
+	def __init__(self, chainring, cog):
+		self.__chainring = chainring
+		self.__cog = cog
+
+	# getter methods to keep initialized variables private
+	@property
+	def chainring(self):
+		return self.__chainring
+
+	@property
+	def cog(self):
+		return self.__cog
+
+	def ratio(self):
+		return self.chainring / float(self.cog)
+
+	def gear_inches(self, diameter):
+		return self.ratio() * diameter
+
+# Wheel now has the diameter method and initializes Gear
+class Wheel:
+	# initialize
+	def __init__(self, rim, tire, chainring, cog):
+		self.__rim = rim
+		self.__tire = tire
+		self.__gear = Gear(chainring, cog)
+
+	@property
+	def rim(self):
+		return self.__rim
+
+	@property
+	def tire(self):
+		return self.__tire
+
+	@property
+	def gear(self):
+		return self.__gear
+
+	def diameter(self):
+		return self.rim + (self.tire * 2)
+
+	def gear_inches(self):
+		return self.gear.gear_inches(self.diameter())
+
 
 if __name__ == '__main__':
-    from POODR import Wheel
-    ## Gear expects variable number of key word arguments
-    new_gear = GearWrapper.gear(chainring=52, cog=11, wheel=Wheel(26, 1.5))
-    #expect -----> 137.0909090909091
-    print(new_gear.gear_inches())
+	## create new instance of Wheel
+	new_wheel = Wheel( 26 , 1.5, 52 , 11 )
+	#expect -----> 137.0909090909091
+	print(new_wheel.gear_inches())
