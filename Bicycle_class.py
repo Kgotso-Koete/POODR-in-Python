240ff87ceac2ddc19e44bea6e29e1ff3c348dc95
Kgotso Koete <kgkoete@gmail.com>
2017-12-09 16:58:50 +0200

    Ch 5 First attempt at a Bicycle sub-class

--- Bicycle_class.py
+++ Bicycle_class.py
@@ -1,30 +1,38 @@
 '''
-Creating a Bicycle class trying to be both road and mountain bike
+First attempt at a Bicycle sub-class
 '''
 
 class Bicycle(object):
 	def __init__(self,**kwargs):
-		self.style = kwargs['style']
 		self.size = kwargs['size']
-		self.tape_color = kwargs.get('tape_color', 'red')
+		self.tape_color = kwargs.get('tape_color', None)
+
+	def spares(self):
+		return {'chain': '10-speed' , 'tire_size': '23' , 'tape_color': self.tape_color}
+
+class MountainBike(Bicycle):
+	def __init__(self,**kwargs):
+		Bicycle.__init__(self,**kwargs)
 		self.front_shock = kwargs['front_shock']
 		self.rear_shock = kwargs['rear_shock']
 
 	def spares(self):
-		if (self.style == 'road'):
-			return {'chain':'10-speed', 'tire_size':'23', 'tape_color':self.tape_color}
-		else:
-			return {'chain':'10-speed', 'tire_size':'2.1', 'rear_shock':self.rear_shock}
+		add_spare = {'front_shock':self.front_shock, 'rear_shock': self.rear_shock}
+		return {**super().spares(), **add_spare}
+
 
 if __name__ == '__main__':
 	# initialize a new bike
-	bike = Bicycle(
-		style = 'mountain',
-		size = 'S',
-		front_shock = 'Manitou',
-		rear_shock = 'Fox')
+	mountain_bike = MountainBike(
+                  size = 'S',
+                  front_shock = 'Manitou',
+                  rear_shock = 'Fox')
 
-	print(bike.spares())
-	# -> {:tire_size   => "2.1",
+	print(mountain_bike.size)
+	 # -> 'S')
+	print(mountain_bike.spares())
+	# -> {:tire_size   => "23",       <- wrong!
 	#     :chain       => "10-speed",
-	#     :rear_shock  => 'Fox'}
+	#     :tape_color  => nil,        <- not applicable
+	#     :front_shock => 'Manitou',
+	#     :rear_shock  => "Fox"}
