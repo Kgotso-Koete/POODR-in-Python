f01c04cd8c6491940fbcc954334884a39116a557
Kgotso Koete <kgkoete@gmail.com>
2017-12-09 16:06:49 +0200

    Ch 5 Bicycle class trying to be both road and mountain bike

--- Bicycle_class.py
+++ Bicycle_class.py
@@ -1,19 +1,30 @@
 '''
-Creating a Bicycle class with attributes/methods to be inherited
+Creating a Bicycle class trying to be both road and mountain bike
 '''
 
 class Bicycle(object):
 	def __init__(self,**kwargs):
+		self.style = kwargs['style']
 		self.size = kwargs['size']
-		self.tape_color = kwargs['tape_color']
+		self.tape_color = kwargs.get('tape_color', 'red')
+		self.front_shock = kwargs['front_shock']
+		self.rear_shock = kwargs['rear_shock']
 
 	def spares(self):
-		return {'chain': '10-speed' , 'tire_size': '23' , 'tape_color': self.tape_color}
+		if (self.style == 'road'):
+			return {'chain':'10-speed', 'tire_size':'23', 'tape_color':self.tape_color}
+		else:
+			return {'chain':'10-speed', 'tire_size':'2.1', 'rear_shock':self.rear_shock}
 
 if __name__ == '__main__':
 	# initialize a new bike
-	bike = Bicycle( size = 'M' ,tape_color = 'red' )
-	print(bike.size)
-	# -> 'M'
+	bike = Bicycle(
+		style = 'mountain',
+		size = 'S',
+		front_shock = 'Manitou',
+		rear_shock = 'Fox')
+
 	print(bike.spares())
-	# -> {:tire_size => "23", 27 # :chain => "10-speed", 28 # :tape_color => "red"}
+	# -> {:tire_size   => "2.1",
+	#     :chain       => "10-speed",
+	#     :rear_shock  => 'Fox'}
