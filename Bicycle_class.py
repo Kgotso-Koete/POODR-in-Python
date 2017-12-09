b00b96205637e14cb67912998391a1e36741f116
Kgotso Koete <kgkoete@gmail.com>
2017-12-09 17:02:51 +0200

    Ch 5 Bicycle sub-class with data hiding

--- Bicycle_class.py
+++ Bicycle_class.py
@@ -4,8 +4,16 @@ First attempt at a Bicycle sub-class
 
 class Bicycle(object):
 	def __init__(self,**kwargs):
-		self.size = kwargs['size']
-		self.tape_color = kwargs.get('tape_color', None)
+		self.__size = kwargs['size']
+		self.__tape_color = kwargs.get('tape_color', None)
+
+	@property
+	def size(self):
+		return self.__size
+
+	@property
+	def tape_color(self):
+		return self.__tape_color
 
 	def spares(self):
 		return {'chain': '10-speed' , 'tire_size': '23' , 'tape_color': self.tape_color}
@@ -13,8 +21,16 @@ class Bicycle(object):
 class MountainBike(Bicycle):
 	def __init__(self,**kwargs):
 		Bicycle.__init__(self,**kwargs)
-		self.front_shock = kwargs['front_shock']
-		self.rear_shock = kwargs['rear_shock']
+		self.__front_shock = kwargs['front_shock']
+		self.__rear_shock = kwargs['rear_shock']
+
+	@property
+	def front_shock(self):
+		return self.__front_shock
+
+	@property
+	def rear_shock(self):
+		return self.__rear_shock
 
 	def spares(self):
 		add_spare = {'front_shock':self.front_shock, 'rear_shock': self.rear_shock}
